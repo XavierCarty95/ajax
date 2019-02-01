c721a9c44c91b9ea048aacfd65f46950101a8d67
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-02-01 00:12:44 +0000

    generators in asyncjavascript

---
+++ ajversion5.js
@@ -0,0 +1,48 @@
+window.onload = function(){
+	
+	genWrap(function*(){
+		
+		var tweets = yield $.get("data/tweets.json");
+		console.log(tweets);
+		var friends = yield $.get("data/friends.json");
+		console.log(friends);
+		var videos = yield $.get("data/videos.json");
+		console.log(videos);
+		
+	});
+	
+	function genWrap(generator){
+		
+		var gen = generator();
+		
+		function handle(yielded) {
+			if(!yielded.done){
+				yielded.value.then(function(data){
+					return handle(gen.next(data));
+				})
+			}
+		}
+		
+		return handle(gen.next());
+		
+	}
+
+};	
+
+
+
+
+
+
+
+// window.onload = function() {
+    
+//     function* gen(){
+//         var x = yield 10; 
+//         console.log(x)    
+//     }
+    
+//     var myGen = gen()
+//     console.log(myGen.next())
+//     console.log(myGen.next(10))
+// }
